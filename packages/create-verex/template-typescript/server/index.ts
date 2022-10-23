e3a118dfbd7cfcac3d3ec35b6c06c52be6355551
Vanaldito <fredimanuelb@gmail.com>
2022-10-22 21:17:20 -0600

    fix: change require for dynamic import in typescript template

--- packages/create-verex/template-typescript/server/index.ts
+++ packages/create-verex/template-typescript/server/index.ts
@@ -3,36 +3,40 @@ import path from "path";
 
 import apiRouter from "./src/routes/api";
 
-const app = express();
+async function main() {
+  const app = express();
 
-if (process.env.VEREX_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "static")));
+  if (process.env.VEREX_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, "static")));
+  }
+
+  app.use("/api/v1", apiRouter);
+
+  app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, process.env.VEREX_HTML_PATH as string));
+  });
+
+  /* Add your routes here */
+
+  if (process.env.VEREX_ENV === "development") {
+    const { Assets } = await import("verex");
+    new Assets().useRouter(app);
+  }
+
+  app.get("/*", (_req, res) => {
+    res
+      .status(404)
+      .sendFile(path.join(__dirname, process.env.VEREX_HTML_PATH as string));
+  });
+
+  const PORT = 5000;
+
+  app.listen(PORT, () => {
+    console.log();
+    console.log(`  App running in port ${PORT}`);
+    console.log();
+    console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+  });
 }
 
-app.use("/api/v1", apiRouter);
-
-app.get("/", (_req, res) => {
-  res.sendFile(path.join(__dirname, process.env.VEREX_HTML_PATH as string));
-});
-
-/* Add your routes here */
-
-if (process.env.VEREX_ENV === "development") {
-  const { Assets } = require("verex");
-  new Assets().useRouter(app);
-}
-
-app.get("/*", (_req, res) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname, process.env.VEREX_HTML_PATH as string));
-});
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log();
-  console.log(`  App running in port ${PORT}`);
-  console.log();
-  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
-});
+main();
